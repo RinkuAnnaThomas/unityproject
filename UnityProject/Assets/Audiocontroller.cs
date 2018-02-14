d5fa6bc8fd578d09f3334233716d9b72541321a5
RinkuAnnaThomas <36261006+RinkuAnnaThomas@users.noreply.github.com>
2018-02-14 11:02:42 +0530

    unityproject

---
+++ UnityProject/Assets/Audiocontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour {
+
+	public Slider volumes;
+	public AudioSource MyVolumes;
+
+	void Update()
+	{
+		MyVolumes.volume = volumes.value;
+	}
+
+
+}
---
+++ UnityProject/Assets/MusicController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Audiocontroller : MonoBehaviour {
+
+	public Slider volume;
+	public AudioSource MyVolume;
+
+	void Update()
+	{
+		MyVolume.volume = volume.value;
+	}
+
+
+}
